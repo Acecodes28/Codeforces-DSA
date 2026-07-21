d953d45e155f1329cc242274a5f9055c381e218e
Anubhav <shawanubhav28@gmail.com>
2026-07-21 21:00:28 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.cpp
@@ -0,0 +1,31 @@
+// buttons
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t; 
+    cin>>t; 
+    while(t--)
+    {
+        int sum=0;
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a>b)
+        {
+            cout<<"First"<<endl;
+        }
+        else if (b>a)
+        cout<<"Second"<<endl;
+        else if (a==b){
+            if(c%2==0)
+            {
+                cout<<"Second"<<endl;
+            }
+            else
+            {
+            cout<< "First"<<endl;
+            }
+        }
+}
+return 0;
+}
