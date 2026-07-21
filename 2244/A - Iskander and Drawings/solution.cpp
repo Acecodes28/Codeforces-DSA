de2160292ea8690a7bc79b97e6fa3082937a1ddb
Anubhav <shawanubhav28@gmail.com>
2026-07-21 21:04:07 +0530

    Add Iskander and Drawings [A] from Codeforces

---
+++ 2244/A - Iskander and Drawings/solution.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+ 
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+ 
+    int max_len = 0;
+    int current_len = 0;
+ 
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == '#') {
+            current_len++;
+            max_len = max(max_len, current_len);
+        } else {
+            current_len = 0;
+        }
+    }
+ 
+    cout << (max_len + 1) / 2 << "
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
